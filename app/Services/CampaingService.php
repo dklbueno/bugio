83b85975cfed66d90b16182791d00e02787848c7
Daniel Bueno <dkbueno@gmail.com>
2024-08-20 18:21:35 -0300

    removendo asterix das consultas

--- app/Services/CampaingService.php
+++ app/Services/CampaingService.php
@@ -13,6 +13,9 @@ class CampaingService
             $owners = [];
             foreach($detail['realty'] as $realty) {
                 foreach($realty['owners'] as $owner) {
+                    if(str_contains($owner['document_number'], '*')) {
+                        continue;
+                    }
                     $url = config('services.eemovel_agro.url') . "/campaing/{$campaingId}/owner";
                     $response = Http::withHeaders([
                                 'x-api-key' => config('services.eemovel_agro.token')
@@ -43,41 +46,4 @@ class CampaingService
         
         return $owners;
     }
-
-    // public static function getOwners($detail, $campaingId)
-    // {
-    //     $owners = Cache::remember('campaing-owners-' . $campaingId, 30, function() use($detail, $campaingId) {
-    //         $data = [];
-    //         foreach($detail['realty'] as $realty) {
-    //             foreach($realty['owners'] as $owner) {
-    //                 $url = config('services.eemovel_agro.url') . "/campaing/{$campaingId}/owner";
-    //                 $response = Http::withHeaders([
-    //                             'x-api-key' => config('services.eemovel_agro.token')
-    //                         ])->post($url, ['document' => $owner['document_number']]);
-
-    //                 if($response->status() == 200) {
-    //                     $data = $response->json()['data'];
-
-    //                     $email = count($data['emails']) ? $data['emails'][0]['email'] : '';
-    //                     $phone = count($data['phones']) ? $data['phones'][0]['phone_number'] : '';
-                                        
-    //                     $data[] = [
-    //                         'name' => $data['name'],
-    //                         'email' => $email,
-    //                         'phone' => $phone,
-    //                         'document' => $data['document_number'],
-    //                         'street' => count($data['addresses']) ? $data['addresses'][0]['street'] : '',
-    //                         'city' => count($data['addresses']) ? $data['addresses'][0]['city'] : '',
-    //                         'address_numner' => count($data['addresses']) ? $data['addresses'][0]['number'] : '',
-    //                         'uf' => count($data['addresses']) ? $data['addresses'][0]['uf'] : ''
-    //                     ];
-    //                 }                
-    //             }
-    //         }
-            
-    //         return $data;
-    //     });   
-        
-    //     return $owners;
-    // }
 }
